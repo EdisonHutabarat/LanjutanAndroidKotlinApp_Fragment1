73f0e2bbce747192a91a7972f717827e48e34524
EdisonHutabarat <edisonhutabaratp17@gmail.com>
2024-10-08 10:40:46 +0700

    Menerapkan view binding

--- app/src/main/java/com/edisonrizal/androidkotlinapp/MainActivity.kt
+++ app/src/main/java/com/edisonrizal/androidkotlinapp/MainActivity.kt
@@ -4,44 +4,52 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
-import android.widget.Button
-import android.widget.TextView
+//import android.widget.Button
+//import android.widget.TextView
+import com.edisonrizal.androidkotlinapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var tvCounter: TextView
-    private lateinit var btnAddNumber: Button
-    private lateinit var btnResetNumber: Button
-    private lateinit var btnSubstractNumber: Button
+    private lateinit var binding: ActivityMainBinding
+
+//    private lateinit var tvCounter: TextView
+//    private lateinit var btnAddNumber: Button
+//    private lateinit var btnResetNumber: Button
+//    private lateinit var btnSubstractNumber: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //layout binding
-        setContentView(R.layout.activity_main)
 
-        //view binding
-        tvCounter = findViewById(R.id.tvCounter)
-        btnAddNumber = findViewById(R.id.btnAddNumber)
-        btnResetNumber = findViewById(R.id.btnResetNumber)
-        btnSubstractNumber = findViewById(R.id.btnSubstractNumber)
+        //layout binding with library view binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        tvCounter.text = "1"
+//        //layout binding
+//        setContentView(R.layout.activity_main)
+//
+//        //view binding
+//        tvCounter = findViewById(R.id.tvCounter)
+//        btnAddNumber = findViewById(R.id.btnAddNumber)
+//        btnResetNumber = findViewById(R.id.btnResetNumber)
+//        btnSubstractNumber = findViewById(R.id.btnSubstractNumber)
+
+        binding.tvCounter.text = "1"
     }
     //fungsi untuk menambah
     fun addNumber(v: View){
-        val currVal = tvCounter.text.toString().toInt()
+        val currVal = binding.tvCounter.text.toString().toInt()
         val nextVal = currVal + 1
-        tvCounter.text = nextVal.toString()
+        binding.tvCounter.text = nextVal.toString()
     }
 
     fun resetNumber(v: View){
-        tvCounter.text = "0"
+        binding.tvCounter.text = "0"
     }
 
     fun substractNumber(v: View) {
-        val currVal = tvCounter.text.toString().toInt()
+        val currVal = binding.tvCounter.text.toString().toInt()
         val nextVal = currVal - 1
-        tvCounter.text = nextVal.toString()
+        binding.tvCounter.text = nextVal.toString()
     }
 }
