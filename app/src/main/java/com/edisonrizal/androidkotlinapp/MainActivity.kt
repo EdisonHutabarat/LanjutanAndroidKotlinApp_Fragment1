db29bde068d9ea8aa36eab7d836e323980370e60
EdisonHutabarat <edisonhutabaratp17@gmail.com>
2024-10-08 10:30:15 +0700

    menambahkan fungsi add, reset, dan substract

--- app/src/main/java/com/edisonrizal/androidkotlinapp/MainActivity.kt
+++ app/src/main/java/com/edisonrizal/androidkotlinapp/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.edisonrizal.androidkotlinapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvCounter: TextView
+    private lateinit var btnAddNumber: Button
+    private lateinit var btnResetNumber: Button
+    private lateinit var btnSubstractNumber: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //layout binding
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        //view binding
+        tvCounter = findViewById(R.id.tvCounter)
+        btnAddNumber = findViewById(R.id.btnAddNumber)
+        btnResetNumber = findViewById(R.id.btnResetNumber)
+        btnSubstractNumber = findViewById(R.id.btnSubstractNumber)
+
+        tvCounter.text = "1"
+    }
+    //fungsi untuk menambah
+    fun addNumber(v: View){
+        val currVal = tvCounter.text.toString().toInt()
+        val nextVal = currVal + 1
+        tvCounter.text = nextVal.toString()
+    }
+
+    fun resetNumber(v: View){
+        tvCounter.text = "0"
+    }
+
+    fun substractNumber(v: View) {
+        val currVal = tvCounter.text.toString().toInt()
+        val nextVal = currVal - 1
+        tvCounter.text = nextVal.toString()
     }
 }
