444872954ccf1cf215e6f00e460e405085c73a48
EdisonHutabarat <edisonhutabaratp17@gmail.com>
2024-10-11 22:59:05 +0700

    EventHandling Example, add OnClick and OnLongClick

--- EventHandlingExample/app/src/main/java/id/ac/polbeng/edisonrizal/eventhandlingexample/MainActivity.kt
+++ EventHandlingExample/app/src/main/java/id/ac/polbeng/edisonrizal/eventhandlingexample/MainActivity.kt
@@ -1,20 +1,40 @@
 package id.ac.polbeng.edisonrizal.eventhandlingexample
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import id.ac.polbeng.edisonrizal.eventhandlingexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        binding.btnDisplayMessage.setOnClickListener(object: View.OnClickListener{
+//            override fun onClick(v: View?) {
+//                Toast.makeText(applicationContext, "Hello World",
+//                    Toast.LENGTH_LONG).show()
+//            }
+//        })
+
+//        Convert to lambda
+
+        binding.btnDisplayMessage.setOnClickListener {
+            Toast.makeText(
+                applicationContext, "Hello World",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+        binding.btnDisplayMessage.setOnLongClickListener {
+            Snackbar.make(binding.rootLayout, "Long Click",
+                Snackbar.LENGTH_LONG).show()
+            true
         }
     }
 }
